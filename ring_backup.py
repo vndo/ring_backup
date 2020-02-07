791cd7960a1d040b2de11271f606961895329627
vndo <njdownes@gmail.com>
2020-02-07 17:19:42 +0000

    Create ring_backup.py

---
+++ ring_backup.py
@@ -0,0 +1,62 @@
+import json
+import getpass
+from pathlib import Path
+
+from ring_doorbell import Ring, Auth
+from oauthlib.oauth2 import MissingTokenError
+
+
+cache_file = Path("test_token.cache")
+
+
+def token_updated(token):
+	cache_file.write_text(json.dumps(token))
+
+
+def otp_callback():
+	auth_code = input("2FA code: ")
+	return auth_code
+
+
+def main():
+	if cache_file.is_file():
+		auth = Auth("MyProject/1.0", json.loads(cache_file.read_text()), token_updated)
+	else:
+		username = input("Username: ")
+		password = getpass.getpass("Password: ")
+		auth = Auth("MyProject/1.0", None, token_updated)
+		try:
+			auth.fetch_token(username, password)
+		except MissingTokenError:
+			auth.fetch_token(username, password, otp_callback())
+
+	ring = Ring(auth)
+	ring.update_data()
+
+	devices = ring.devices()
+	print(devices)
+
+	doorbells = devices["doorbots"]
+	chimes = devices["chimes"]
+	stickup_cams = devices["stickup_cams"]
+
+	print(doorbells)
+	#print(chimes)
+	#print(stickup_cams)
+	x = 0
+	y = 0
+	limit = 100
+	doorbell = devices['doorbots'][0]
+	typelist = {
+		"motion",
+		"ding",
+	}
+	for rt in typelist:
+		print(rt)
+		for i in doorbell.history(limit=limit, kind=rt):
+			print(i['id'])
+			doorbell.recording_download(doorbell.history(limit=limit, kind=rt)[x]['id'],filename=str(i['created_at'].strftime("%m-%d-%Y-%H-%M-%S"))+'.mp4',override=True)
+			x+=1
+
+if __name__ == "__main__":
+	main()
